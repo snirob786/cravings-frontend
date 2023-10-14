4c834320458a250d8aa0202ad71a57e513b0c328
Saifur Rahman Nirob <snirob786@gmail.com>
2023-10-14 22:47:26 +0600

    feat: header structure organized

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,15 @@
 import React from 'react'
-import { UploadOutlined, UserOutlined, VideoCameraOutlined, AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import { Layout, Menu, theme } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
+import DashboardHeader from '@/components/dashboard-header';
+const { Sider } = Layout;
 
 export default function index() {
 
-    const items = [
-        {
-            label: 'Navigation One',
-            key: 'mail',
-            icon: <MailOutlined />,
-        },
-        {
-            label: 'Navigation Two',
-            key: 'app',
-            icon: <AppstoreOutlined />,
-        },
-        {
-            label: 'Navigation Three - Submenu',
-            key: 'SubMenu',
-            icon: <SettingOutlined />,
-            children: [
-                {
-                    type: 'group',
-                    label: 'Item 1',
-                    children: [
-                        {
-                            label: 'Option 1',
-                            key: 'setting:1',
-                        },
-                        {
-                            label: 'Option 2',
-                            key: 'setting:2',
-                        },
-                    ],
-                },
-                {
-                    type: 'group',
-                    label: 'Item 2',
-                    children: [
-                        {
-                            label: 'Option 3',
-                            key: 'setting:3',
-                        },
-                        {
-                            label: 'Option 4',
-                            key: 'setting:4',
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            label: (
-                <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                    Navigation Four - Link
-                </a>
-            ),
-            key: 'alipay',
-        },
-    ];
 
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
     return (
         <Layout>
-            <Sider
+            {/* <Sider
                 breakpoint="lg"
                 collapsedWidth="0"
                 onBreakpoint={(broken) => {
@@ -89,22 +32,9 @@ export default function index() {
                         }),
                     )}
                 />
-            </Sider>
+            </Sider> */}
             <Layout>
-                <Header style={{ padding: 0, background: colorBgContainer }} >
-                    <div className="demo-logo">
-                        Test
-                    </div>
-                    <Menu
-                        mode="horizontal"
-                        defaultSelectedKeys={['4']}
-                        items={items}
-                    />
-                </Header>
-                <Content style={{ margin: '24px 16px 0' }}>
-                    <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>content</div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+                <DashboardHeader />
             </Layout>
         </Layout>
     )
